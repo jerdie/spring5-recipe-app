ad0f1929d3e1b299b316b05a5034133714c4c71c
jerdie <5143814+jerdie@users.noreply.github.com>
2021-09-06 23:24:10 +0800

    add webjars add link to show recipe

--- src/test/java/guru/springframework/recipe/RecipeServiceImplTest.java
+++ src/test/java/guru/springframework/recipe/RecipeServiceImplTest.java
@@ -7,9 +7,13 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
 
 public class RecipeServiceImplTest {
 
@@ -25,6 +29,21 @@ public class RecipeServiceImplTest {
     }
 
     @Test
+    public void getRecipeByIdTest() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        Optional<Recipe> recipeOptional = Optional.of(recipe);
+
+        when(recipeRepository.findById(anyLong())).thenReturn(recipeOptional);
+
+        Recipe recipeReturned = recipeService.findById(1L);
+
+        assertNotNull("Null recipe returned", recipeReturned);
+        verify(recipeRepository, times(1)).findById(anyLong());
+        verify(recipeRepository, never()).findAll();
+    }
+
+    @Test
     public void getRecipes() {
         Recipe recipe = new Recipe();
         HashSet<Recipe> recipes = new HashSet<>();
@@ -35,6 +54,7 @@ public class RecipeServiceImplTest {
         assertEquals(set.size(), 1);
 
         Mockito.verify(recipeRepository, Mockito.times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
 
     }
 }
