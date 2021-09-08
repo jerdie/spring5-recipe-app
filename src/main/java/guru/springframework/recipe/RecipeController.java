06579fd0b27b38effc6957b4b38a5b1bfaa75be2
jerdie <5143814+jerdie@users.noreply.github.com>
2021-09-08 23:34:18 +0800

    Create new recipe and show

--- src/main/java/guru/springframework/recipe/RecipeController.java
+++ src/main/java/guru/springframework/recipe/RecipeController.java
@@ -2,7 +2,9 @@ package guru.springframework.recipe;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -21,5 +23,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model){
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model){
+        model.addAttribute("recipe",new RecipeCommand());
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping("recipe")
+    @PostMapping
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand){
+        RecipeCommand recipeCommand1 = recipeService.saveRecipeCommand(recipeCommand);
+        return "redirect:show/" + recipeCommand1.getId();
     }
 }
