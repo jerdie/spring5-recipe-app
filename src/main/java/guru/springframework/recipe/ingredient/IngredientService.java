292dd9912f06a89259e4995097c6f64de749981d
jerdie <5143814+jerdie@users.noreply.github.com>
2021-09-11 23:27:16 +0800

    Delete ingredient

--- src/main/java/guru/springframework/recipe/ingredient/IngredientService.java
+++ src/main/java/guru/springframework/recipe/ingredient/IngredientService.java
@@ -3,4 +3,5 @@ package guru.springframework.recipe.ingredient;
 public interface IngredientService {
     IngredientCommand findByRecipeIdAndIngredientId(Long recipeId, Long id);
     IngredientCommand saveIngredientCommand(IngredientCommand command);
+    void deleteById(Long recipeId, Long idToDelete);
 }
